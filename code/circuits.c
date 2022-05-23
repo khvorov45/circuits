5f38c409252319497a13c460922dfdf35ea3ee87
Arseniy Khvorov <khvorov45@gmail.com>
2022-05-23 10:40:02 +1000

    take pin drawing logic out

--- code/circuits.c
+++ code/circuits.c
@@ -56,26 +56,72 @@ typedef struct Chip {
 	i32 inputCount, outputCount;
 } Chip;
 
+typedef enum Direction {
+	Direction_Top,
+	Direction_Bottom,
+	Direction_Left,
+	Direction_Right,
+} Direction;
+
+function void
+drawChipPins(Renderer* renderer, Rect2i bodyRect, i32 pinCount, Direction dir) {
+
+	v4 pinCol = {1, 1, 0, 1};
+	i32 pinGirth = 2;
+	i32 pinLength = 50;
+
+	i32 pinStep = bodyRect.dim.x / (pinCount + 1);
+
+	v2i bodyRectBottomright = getRectBottomright(bodyRect);
+	Rect2i curPinRect = {0};
+	switch (dir) {
+	case Direction_Top: {
+		curPinRect.topleft.x = bodyRect.topleft.x + pinStep; 
+		curPinRect.topleft.y = bodyRect.topleft.y - pinLength;
+	} break;
+	case Direction_Bottom: {
+		curPinRect.topleft.x = bodyRect.topleft.x + pinStep; 
+		curPinRect.topleft.y = bodyRectBottomright.y;
+	} break;
+	case Direction_Left: {
+		curPinRect.topleft.x = bodyRect.topleft.x - pinLength; 
+		curPinRect.topleft.y = bodyRect.topleft.y + pinStep;
+	} break;
+	case Direction_Right: {
+		curPinRect.topleft.x = bodyRectBottomright.x; 
+		curPinRect.topleft.y = bodyRect.topleft.y + pinStep;
+	} break;
+	};
+
+	i32* toIncrement = 0;
+	switch (dir) {
+	case Direction_Top: case Direction_Bottom: {
+		toIncrement = &curPinRect.topleft.x;
+		curPinRect.dim.x = pinGirth; 
+		curPinRect.dim.y = pinLength;
+	} break;
+	case Direction_Right: case Direction_Left: {
+		toIncrement = &curPinRect.topleft.y;
+		curPinRect.dim.x = pinLength; 
+		curPinRect.dim.y = pinGirth;
+	} break;
+	}
+
+	for (i32 inputIndex = 0; inputIndex < pinCount; inputIndex += 1) {
+
+		drawRect(renderer, curPinRect, pinCol);
+		*toIncrement += pinStep;
+	}
+}
+
 function void 
 drawChip(Renderer* renderer, Chip chip, v2i center) {
 	v2i dim = {100, 100};
 	Rect2i bodyRect = rectCenterDim(center, dim);
 	v4 col = {1, 0, 0, 1};
 	drawRect(renderer, bodyRect, col);
-
-	v2i bodyRectBottomright = getRectBottomright(bodyRect);
-
-	v4 pinCol = {1, 1, 0, 1};
-	i32 pinGirth = 2;
-	i32 pinLength = 50;
-
-	i32 pinStep = bodyRect.dim.x / (chip.inputCount + 1);
-	Rect2i curPinRect = {{bodyRect.topleft.x + pinStep, bodyRectBottomright.y}, {pinGirth, pinLength}};
-	for (i32 inputIndex = 0; inputIndex < chip.inputCount; inputIndex += 1) {
-
-		drawRect(renderer, curPinRect, pinCol);
-		curPinRect.topleft.x += pinStep;
-	}
+	drawChipPins(renderer, bodyRect, chip.inputCount, Direction_Bottom);
+	drawChipPins(renderer, bodyRect, chip.outputCount, Direction_Top);
 }
 
 function void
